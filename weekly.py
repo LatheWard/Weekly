5383bc13217454970e75258614be458844517ff7
LatheWard <72397286+LatheWard@users.noreply.github.com>
2022-07-09 12:40:44 -0500

    making data frame and setting columns in list

--- weekly.py
+++ weekly.py
@@ -1,25 +1,42 @@
-import csv, pandas as pd
+import pandas as pd
 
-filename = "RegionDelivery.csv"
+# filename = "RegionDelivery.csv"
 
-fields = []
-rows = []
+# fields = []
+# rows = []
 
-with open(filename, 'r') as csvfile:
-    csvreader = csv.reader(csvfile)
-    fields = next(csvreader)
+# with open(filename, 'r') as csvfile:
+#     csvreader = csv.reader(csvfile)
+#     fields = next(csvreader)
 
-    for row in csvreader:
-        rows.append(row)
+#     for row in csvreader:
+#         rows.append(row)
 
-    print("Total no. of rows: %d"%(csvreader.line_num))
+#     print("Total no. of rows: %d"%(csvreader.line_num))
 
 
-print('Field names are:' + ', '.join(field for field in fields))
-print('\nFirst 5 rows are:\n')
-for row in rows[:5]:
-    for col in row:
-        print("%10s"%col,end=" "),
-    print('\n')
+# print('Field names are:' + ', '.join(field for field in fields))
+# print('\nFirst 5 rows are:\n')
+# for row in rows[:5]:
+#     for col in row:
+#         print("%10s"%col,end=" "),
+#     print('\n')
 
-# pd.read_csv("RegionDelivery.csv")
+df = pd.read_csv("RegionDelivery.csv")
+
+cols = ['DeliveryID', 
+        'Last Name', 
+        'First Name', 
+        'MiddleName', 
+        'MedicaidNo', 
+        'ActivityDate', 
+        'DeliveryTime', 
+        'MealType', 
+        'Units', 
+        'Status', 
+        'Unnamed: 10', 
+        'Unnamed: 11', 
+        'Unnamed: 12', 
+        'Unnamed: 13']
+
+print(cols)
