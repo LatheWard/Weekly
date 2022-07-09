b412cb77ac70ca932a4ae7e0035a7059cb7e0db6
LatheWard <72397286+LatheWard@users.noreply.github.com>
2022-07-09 11:59:20 -0500

    about to use pandas to read and manipulate

---
+++ weekly.py
@@ -0,0 +1,25 @@
+import csv, pandas as pd
+
+filename = "RegionDelivery.csv"
+
+fields = []
+rows = []
+
+with open(filename, 'r') as csvfile:
+    csvreader = csv.reader(csvfile)
+    fields = next(csvreader)
+
+    for row in csvreader:
+        rows.append(row)
+
+    print("Total no. of rows: %d"%(csvreader.line_num))
+
+
+print('Field names are:' + ', '.join(field for field in fields))
+print('\nFirst 5 rows are:\n')
+for row in rows[:5]:
+    for col in row:
+        print("%10s"%col,end=" "),
+    print('\n')
+
+# pd.read_csv("RegionDelivery.csv")
