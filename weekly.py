451c36b99282a0b18594b78d4efb00acbdefeb57
LatheWard <lathelward@gmail.com>
2022-07-15 15:32:34 -0500

    Still floating ideas around on teh format

--- weekly.py
+++ weekly.py
@@ -25,14 +25,7 @@ rf_df.rename(columns={"Unnamed: 11": "County"}, inplace=True)
 rf_df.rename(columns={"Unnamed: 12": "Agent"}, inplace=True)
 rf_df.rename(columns={"Unnamed: 13": "City"}, inplace=True)
 
-sdf = pd.DataFrame #Sum Data Frame
-
-
-FZ_ST = df.loc[df['MealType'] == "FROZEN 5PK D2D (MW)"]
-
-FZ_MW = df.loc[df['MealType'] == "FROZEN 5PK D2D (STATE)"]
-
-sumcols = ['Abbeville',
+stsumrows = ['Abbeville',
            'Philadelphia',
            'Aberdeen',
            'Pontotoc', 
@@ -44,11 +37,31 @@ sumcols = ['Abbeville',
            'Pontotoc',
            'Union']
 
+stsumcols = ['Delivered',
+             'Not Delivered'
+             ]
+
+
+
+FZ_ST = df.loc[df['MealType'] == "FROZEN 5PK D2D (MW)"]
+
+# FZ_MW = df.loc[df['MealType'] == "FROZEN 5PK D2D (STATE)"]  
+
+# site = df.loc[df['City'].str.contains("Abbe")]
+
+medicaiddf = pd.DataFrame(columns = stsumcols, index=stsumrows)
+statedf = pd.DataFrame(columns = stsumcols, index=stsumrows)
+
+
+for i in range(len(df)):
+        if df['MealType'] == "FROZEN 5PK D2D (MW)":
+                if site:
+                        sdf.append
+        
+
 
 # print(rf_df.iloc[: , :])
 
 # rf_df.to_excel('summary.xlsx', sheet_name='Report')
-
-
 
 # Open file from 'Summarize' folder, get a sum for all categories, create new excel or csv file, put sum's under columns in file
