34dbc9bcc54f43761924491ed95fd787d015b1ac
LatheWard <lathelward@gmail.com>
2022-07-15 09:32:04 -0500

    Filtering meal types

--- weekly.py
+++ weekly.py
@@ -1,8 +1,7 @@
 import pandas as pd
 import numpy as np
 
-
-
+# 
 df = pd.read_csv("RegionDelivery.csv")
 
 
@@ -26,9 +25,16 @@ rf_df.rename(columns={"Unnamed: 11": "County"}, inplace=True)
 rf_df.rename(columns={"Unnamed: 12": "Agent"}, inplace=True)
 rf_df.rename(columns={"Unnamed: 13": "City"}, inplace=True)
 
+sdf = pd.DataFrame #Sum Data Frame
 
-print(rf_df.iloc[: , :])
+PP = df.loc[df['MealType'] == "FROZEN 5PK D2D (MW)"]
+PL = df.loc[df['MealType'] == "FROZEN 5PK D2D (MW)"]
+FZ = df.loc[df['MealType'] == "FROZEN 5PK D2D (MW)"]
+SS = df.loc[df['MealType'] == "FROZEN 5PK D2D (MW)"]
+# print(rf_df.iloc[: , :])
 
-rf_df.to_csv('summary.csv')
+# rf_df.to_excel('summary.xlsx', sheet_name='Report')
+
+
 
 # Open file from 'Summarize' folder, get a sum for all categories, create new excel or csv file, put sum's under columns in file
