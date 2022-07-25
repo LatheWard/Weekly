e15d286e47e51112c6eee0962284ca18bd0f4428
LatheWard <lathelward@gmail.com>
2022-07-25 10:06:16 -0500

    usig tkinter, changed col d width, started format loop at row 1

--- weekly.py
+++ weekly.py
@@ -3,9 +3,9 @@ import pandas as pd
 import numpy as np
 from re import IGNORECASE
 from tkinter.filedialog import askopenfilename
-# filename = askopenfilename()
+filename = askopenfilename(filetypes=[("Excel files", ".xlsx .csv")])
 
-df = pd.read_csv("RegionDelivery (2).csv")
+df = pd.read_csv(filename)
 
 # cities = ['Abbeville','Philadelphia','Aberdeen','Pontotoc', 'Calhoun','Chickasaw',
 # 'Itawamba','Lafayette','Monroe','Pontotoc','Union']
@@ -13,10 +13,8 @@ df = pd.read_csv("RegionDelivery (2).csv")
 # "FROZEN 5PK D2D (MW)"
 # "FROZEN 5PK D2D (STATE)"
 
-
 rdf = df.groupby(['MealType', 'City', 'Status'])['DeliveryID'].count()
 rdf.to_frame()
-
 
 writer = pd.ExcelWriter('Sum.xlsx', engine='xlsxwriter')
 rdf.to_excel(writer, sheet_name="Summary")
@@ -30,12 +28,13 @@ format1 = workbook.add_format({'bg_color': '#9AD7A4',
 format2 = workbook.add_format({'bg_color': '#F0CA86',
                                'font_color': '#000000'})
 
-for i in range(len(rdf)): 
+for i in range(1, len(rdf)): 
     worksheet.set_row(i, cell_format=(format1 if i%2==0 else format2))   
 
 worksheet.set_column('A:A', 25)
 worksheet.set_column('B:B', 25)
 worksheet.set_column('C:C', 25)
+worksheet.set_column('D:D', 19)
 
 writer.save()
 
