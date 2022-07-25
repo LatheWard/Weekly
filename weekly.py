602af2a3cf99314d639a587a3044cffdd17b7a0c
LatheWard <lathelward@gmail.com>
2022-07-25 09:39:22 -0500

    formatting every row, blue even rows, gold odd rows

--- weekly.py
+++ weekly.py
@@ -3,23 +3,12 @@ import pandas as pd
 import numpy as np
 from re import IGNORECASE
 from tkinter.filedialog import askopenfilename
-filename = askopenfilename()
+# filename = askopenfilename()
 
 df = pd.read_csv("RegionDelivery (2).csv")
 
-
-# cities = ['Abbeville',
-# 'Philadelphia',
-# 'Aberdeen',
-# 'Pontotoc', 
-# 'Calhoun',
-# 'Chickasaw',
-# 'Itawamba',
-# 'Lafayette',
-# 'Monroe',
-# 'Pontotoc',
-# 'Union']
-
+# cities = ['Abbeville','Philadelphia','Aberdeen','Pontotoc', 'Calhoun','Chickasaw',
+# 'Itawamba','Lafayette','Monroe','Pontotoc','Union']
 
 # "FROZEN 5PK D2D (MW)"
 # "FROZEN 5PK D2D (STATE)"
@@ -33,24 +22,16 @@ writer = pd.ExcelWriter('Sum.xlsx', engine='xlsxwriter')
 rdf.to_excel(writer, sheet_name="Summary")
 workbook = writer.book
 worksheet = writer.sheets["Summary"]
-#Blue
-format1 = workbook.add_format({'bg_color': '#00FFFF',
-                               'font_color': '#000000'})
-#Yellow
-format2 = workbook.add_format({'bg_color': '#FFFF00',
-                               'font_color': '#000000'})
-                            
-start_row = 0
-start_col = 0
-end_row = len(rdf)
-end_col = 4
 
-worksheet.conditional_format(start_row, start_col, end_row, end_col,
-                             {'type':  'cell',
-                              'criteria': '>',
-                              'value':    0,
-                              'format':   format1})
-  
+#Blue
+format1 = workbook.add_format({'bg_color': '#9AD7A4',
+                               'font_color': '#000000'})
+#Gold
+format2 = workbook.add_format({'bg_color': '#F0CA86',
+                               'font_color': '#000000'})
+
+for i in range(len(rdf)): 
+    worksheet.set_row(i, cell_format=(format1 if i%2==0 else format2))   
 
 worksheet.set_column('A:A', 25)
 worksheet.set_column('B:B', 25)
