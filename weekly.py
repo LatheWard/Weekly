fca37148cf1b687d66ed012c721a9b65edba329b
LatheWard <lathelward@gmail.com>
2022-07-25 14:41:42 -0500

    Took out coloration, aligned totals to left

--- weekly.py
+++ weekly.py
@@ -22,26 +22,18 @@ writer = pd.ExcelWriter('Sum.xlsx', engine='xlsxwriter')
 rdf.to_excel(writer, sheet_name="Summary")
 workbook = writer.book
 worksheet = writer.sheets["Summary"]
-
-#Blue
-format1 = workbook.add_format({'bg_color': '#9AD7A4',
-                               'font_color': '#000000'})
-#Gold
-format2 = workbook.add_format({'bg_color': '#F0CA86',
-                               'font_color': '#000000'})
-            
+           
 #Align Left
 formatL = workbook.add_format({'bold': True})
 formatL.set_align('left')
   
 for i in range(1, len(rdf)): 
-    worksheet.set_row(i, cell_format=(format1 if i%2==0 else format2))  
-
+    worksheet.set_row(i, cell_format=(formatL))
 
 worksheet.set_column('A:A', 25)
 worksheet.set_column('B:B', 25)
 worksheet.set_column('C:C', 25)
-worksheet.set_column('D:D', 17, formatL)
+worksheet.set_column('D:D', 15)
 
 worksheet.write(0,6, 'Start Date')
 worksheet.write(0,5, 'Last Date')
